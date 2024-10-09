96fb2b3718dfba2ba6080d8b181a0062884ba59a
Zhaoyu Wang <e1326002@u.nus.edu>
2024-10-09 03:17:42 +0000

    answer to Q5

--- src/App.jsx
+++ src/App.jsx
@@ -293,7 +293,7 @@ class Homepage extends React.Component {
 	super();
 	}
 	render(){
-    const totalSeats = 10;
+    const totalSeats = 50;
     const freeSeats = totalSeats - this.props.travellers.length;
 	return (
 	<div>
